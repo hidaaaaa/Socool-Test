b625f736681e9903829a57648ba35bf69b8b79c7
hida <minuskg123@gmail.com>
2021-10-02 12:25:42 +0700

    Auth

---
+++ src/feature/ForgotFeature/index.jsx
@@ -0,0 +1,12 @@
+import ForgotContent from "commons/ForgotContent";
+import React from "react";
+
+const ForgotFeature = () => {
+	return (
+		<div className="signInFeature">
+			<ForgotContent />
+		</div>
+	);
+};
+
+export default ForgotFeature;
